2ceef7d5931bfa252a85d904455864477d746e08
Daniel <danielcollfdz@gmail.com>
2023-04-02 18:39:38 +0200

    Commit 3 TDD1 Daniel Coll

--- src/Coche.java
+++ src/Coche.java
@@ -1,4 +1,15 @@
 public class Coche {
 
     public int velocidad;
+
+    public void acelerar(int aceleración) {
+        velocidad = velocidad + aceleración;
+    }
+
+    public void decelerar(int deceleración) {
+        velocidad = velocidad - deceleración;
+        if (velocidad<0){
+            velocidad=0;
+        }
+    }
 }
